f354a165b111ca93d460735bc82c982c90e91ccd
Matthijs <matthijs_mandjes@hotmail.com>
2017-09-12 15:42:48 +0200

    Added profile show page

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,26 +1,43 @@
 class ProfilesController < ApplicationController
-  before_action :authenticate_user!, except: [:show]
-  def show
+    before_action :set_profile, only: [:edit, :update, :show]
+    before_action :authenticate_user!
 
-  end
-
-  def new
-    @profile = Profile.new
-  end
-  def create
-
-    @profile = current_user.profiles.build(profile_params)
-    if @profile.save
-      redirect_to root_path
-    else
-      render 'form'
+    def new
+      @profile = Profile.new
     end
-  end
 
-  private
+    def show
 
-  def profile_params
-    profile.require(:profile).permit(:first_name, :last_name, :age, :city, :address, :postal_code, :billing_info)
-  end
+    end
+
+    def create
+      @profile = current_user.build_profile(profile_params)
+
+      if @profile.save
+        redirect_to profile_path(@profile.id), notice: "Profile successfully created"
+      else
+        render :new
+      end
+    end
+
+    def edit
+    end
+
+    def update
+      if @profile.update(profile_params)
+        redirect_to root_path, notice: "Profile successfully updated"
+      else
+        render :edit
+      end
+    end
+
+    private
+      def set_profile
+        @profile = current_user.profile
+      end
+
+      def profile_params
+        params.require(:profile).permit(:first_name, :last_name, :bio)
+      end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   root to: 'catalogues#show'
 
   resources :users, only: [:show]
-  resources :profiles, only: [:new, :edit, :create, :update]
+  resources :profiles
 
   resources :catalogues, only: [:show] do
     resources :products, only: [:show]

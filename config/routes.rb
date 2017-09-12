93f55ce120c5731cdd4405c86967019167041035
MimiMagusin <mimi_magusin@hotmail.com>
2017-09-12 12:01:14 +0200

    Set up routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,13 @@
 Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  root to: 'catalogue#show'
+
+  resources :users, only: [:show]
+  resources :profiles, only: [:new, :edit, :create, :update]
+  resources :catalogues, only: [:show] do
+    resources :products, only: [:show]
+  end
+
 end

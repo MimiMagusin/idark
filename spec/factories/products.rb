f2f82b3b50c3b9dd89bb58b9486522396b5c370a
EAPCMC <E.A.PEENE@GMAIL.com>
2017-09-12 13:58:50 +0200

    Updated factories with faker data

--- spec/factories/products.rb
+++ spec/factories/products.rb
@@ -1,8 +1,8 @@
 FactoryGirl.define do
   factory :product do
-    name "MyString"
-    price "9.99"
-    image "MyString"
-    description "MyText"
+    name          { Faker::Beer.name }
+    price         "5.00"
+    image         "Picture_of_product"
+    description   "This is a beautiful ID. Buy it."
   end
 end

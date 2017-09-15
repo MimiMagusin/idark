1ef400466a7cafe8f2608462f2c17c17c0a6c4b7
Matthijs <matthijs_mandjes@hotmail.com>
2017-09-15 14:32:07 +0200

    Add index page

--- app/controllers/catalogues_controller.rb
+++ app/controllers/catalogues_controller.rb
@@ -1,6 +1,10 @@
 class CataloguesController < ApplicationController
   before_action :authenticate_user!
 
+  def index
+    
+  end
+
   def show
     @products = Product.all
   end
--- config/routes.rb
+++ config/routes.rb
@@ -2,12 +2,12 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  root to: 'catalogues#show'
+  root to: 'catalogues#index'
 
   resources :users, only: [:show]
   resources :profiles, except: [:destroy]
 
-  resources :catalogues, only: [:show] do
+  resources :catalogues, only: [:show, :index] do
     resources :products, only: [:show]
   end
 
